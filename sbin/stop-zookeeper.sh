8806693a77a785df12b7db2496a0fafbdabd0999
Daniel Lopez <de.lopezbarron@gmail.com>
2019-08-17 18:07:16 -0500

    Adding a full stop-zookeeper

--- sbin/stop-zookeeper.sh
+++ sbin/stop-zookeeper.sh
@@ -11,7 +11,7 @@ fi
 
 for ip in $(cat "${RAMCLOUD_HOME}/conf/coordinators" "${RAMCLOUD_HOME}/conf/servers")
 do
-  script="${RAMCLOUD_HOME}/zookeeper/bin/zkServer.sh stop"
+  script="kill -9 \$(jps -lm | grep zookeeper | cut -d' ' -f1)"
   ssh $ip "${script}" > /dev/null
 done
 
